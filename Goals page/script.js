c9287badb2f13c47367b8385e050a80521bb04ec
Muzammil Khan <muzammilsk90@gmail.com>
2021-08-02 19:13:22 +0530

    goals page completed

--- Goals page/script.js
+++ Goals page/script.js
@@ -40,8 +40,114 @@ reset_btn.addEventListener('click',()=>{
     addiction_badge.innerHTML="0/30 days"
 })
 
+//date code
+var timebadge=document.getElementsByTagName("span")[0];
+let today = new Date()
+var date1 = new Date("07/31/2021");
+const date2 = new Date();
+console.log(date2);
+console.log(date1);
+const diffTime=date2.getTime()-date1.getTime();
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+console.log(diffTime + " milliseconds");
+console.log(diffDays + " days");
+timebadge.innerHTML=diffDays+" days" ;
 
 
 
+
+//webdevelopment part code
+var webdev_checkboxes=document.getElementsByClassName("webdev");
+var webdevcount=0;
+console.log(webdev_checkboxes.length);
+var web_progress=document.getElementById("webdev-progress");
+web_progress.style.width=localStorage.getItem("webwidth")+"%";
+
+
+
+for(var j=0;j<webdev_checkboxes.length;j++)
+{
+    if(localStorage.getItem("cb"+j)=="checked")
+    {
+        webdev_checkboxes[j].checked=true;
+    }
+    else
+    {
+        webdev_checkboxes[j].checked=false;
+    }
+}
+
+function countincre()
+{
+    webdevcount=0;
+    for(var i=0;i<webdev_checkboxes.length;i++)
+    {
+        if(webdev_checkboxes[i].checked==true)
+        {
+            webdevcount++;
+            var checked="cb"+i;
+            localStorage.setItem(checked,"checked");
+        }
+        else
+        {
+            var unchecked="cb"+i;
+            localStorage.setItem(unchecked,"unchecked");
+        }
+
+    }
+    var progress_widht=webdevcount/webdev_checkboxes.length*100;
+    localStorage.setItem("webwidth",progress_widht);
+    web_progress.style.width=localStorage.getItem("webwidth")+"%";
+    
+
+}
+
+
+//quran memorization part
+var qucheckboxes=document.getElementsByClassName("qu-check");
+var qucheckcount=0;
+var quranprogress=document.getElementById("quran-progress");
+quranprogress.style.width=localStorage.getItem("quran-progress-width")+"%";
+
+//code for storing the states of the checkboxes
+
+
+function quincre()
+{
+
+    qucheckcount=0;
+    for(var k=0;k<qucheckboxes.length;k++)
+    {
+        
+        if(qucheckboxes[k].checked==true)
+        {
+            var checked="qc"+[k];
+            qucheckcount++;
+            localStorage.setItem(checked,"checked");
+        }
+        else
+        {
+            var unchecked="qc"+[k];
+            localStorage.setItem(unchecked,"unchecked");
+        }
+    }
+    var queprogresswidht=qucheckcount/qucheckboxes.length*100;
+    console.log(queprogresswidht);
+    localStorage.setItem("quran-progress-width",queprogresswidht);
+    quranprogress.style.width=localStorage.getItem("quran-progress-width")+"%";
+
+}
+
+for(var m=0;m<qucheckboxes.length;m++)
+{
+    if(localStorage.getItem("qc"+m)=="checked")
+    {
+        qucheckboxes[m].checked=true;
+    }
+    else
+    {
+        qucheckboxes[m].checked=false;
+    }
+}
 
 
