1097669c5343a12504ab663091e1e72e76b31d33
Muzammil Khan <muzammilsk90@gmail.com>
2021-08-07 17:57:52 +0530

    projects organized

--- javascript trials/script.js
+++ javascript trials/script.js
@@ -1,8 +1,1 @@
-var para=document.getElementsByTagName("p")[0];
-function hiielement()
-{
-    if(para.innerHTML==="0")
-     para.innherHTML="1"
-    else
-     para.innherHTML="0"
-}
+document.getElementById("hello").innerText="hello guys";
--- responsive-navigaton-bar/script.js
+++
@@ -1,5 +0,0 @@
-var toggle=document.getElementsByTagName("button");
-var navbar_container=document.getElementsByClassName("navbar-items")[0]
-toggle.addEventListener('click',()=>{
-console.log(navbar_container)
-})
